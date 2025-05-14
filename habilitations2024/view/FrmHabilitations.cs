7d90a37faab7c7177425b67c99cd0f3ade4ee019
ka-ms-29 <mostaghfera.ka@gmail.com>
2025-05-14 07:02:50 +0200

    Phase1 Création des paquetages et classes, début de code de bddmanager

---
+++ habilitations2024/view/FrmHabilitations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace habilitations2024.view
+
+{
+    public partial class FrmHabilitations : Form
+    {
+        public FrmHabilitations()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+            string connectionString = "Server=localhost;Database=habilitations;User Id=habilitations;Password=motdepasseuser;";
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+                MessageBox.Show("Connexion réussie !");
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
+
+
+        }
+        
+    }
+}
